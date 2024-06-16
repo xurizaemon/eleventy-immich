74322952904ccd9ee4645293748b9fa083429137
Chris Burgess <chris@giantrobot.co.nz>
2024-06-16 14:41:26 +1200

    Use variables to introduce e2e test data
    This removes the requirement for tests to interact with a specific Immich instance

--- test/test.js
+++ test/test.js
@@ -3,13 +3,16 @@ const { immichAlbumShortcode, immichImageShortcode } = require("../immich.js");
 
 test("Test album shortcode output", async t => {
   let html = await immichAlbumShortcode('ef43ecca-f88d-4f5a-b590-533d0611e045');
-  t.regex(html, /<div class="immich-album"><h2>Lomo Baby<\/h2>/);
-  t.regex(html, /src="\/media\/img\/AitV6V6-2X-300.jpeg"/);
-  t.regex(html, /<p>A few selected photos on the Baby Lomo.<\/p>/);
+  let reTitle = new RegExp(`<div class="immich-album"><h2>${process.env.IMMICH_TEST_ALBUM_TITLE}</h2>`);
+  let reDescription = new RegExp(`<p>${process.env.IMMICH_TEST_ALBUM_DESCRIPTION}</p>`);
+  t.regex(html, reTitle);
+  t.regex(html, reDescription);
 });
 
 test("Test image shortcode output", async t => {
   let html = await immichImageShortcode('f2738c5a-2583-4ac7-b034-73c200fdc620');
-  t.regex(html, /<img alt="Mum and me in her garden"/);
-  t.regex(html, /src="\/media\/img\/AitV6V6-2X-300.jpeg"/);
+  let reAlt = new RegExp(`<img alt="${process.env.IMMICH_TEST_IMAGE_DESCRIPTION}"`);
+  let reSrc = new RegExp('src="/media/img/');
+  t.regex(html, reAlt);
+  t.regex(html, reSrc);
 });
