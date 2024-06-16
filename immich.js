866e31c3fcc8c353751a672346ca271a4287729c
Chris Burgess <chris@giantrobot.co.nz>
2024-06-16 14:22:22 +1200

    Refactor API and HTML output to support separated testing
    My plan here was to test separately "does the Immich endpoint return
    sufficiently expected data" and "does passing the data to an HTML render
    function output the expected string(s)".
    
    I now realise that to test the latter without an Immich endpoint, I will
    need to encode both API returned data _and the asset file itself_ as
    fixtures of some kind. And that doesn't need to happen today.

--- immich.js
+++ immich.js
@@ -8,16 +8,16 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 const Image = require("@11ty/eleventy-img");
 
 /**
- * Fetches an album and renders each asset.
+ * Fetches album data by Immich album UUID.
  *
- * @param uuid
- * @returns {Promise<string>}
+ * @param {string} uuid - The UUID of the album.
+ * @return {Promise<Object>} - A promise that resolves to the album data.
  */
-async function immichAlbumShortcode(uuid) {
+async function immichGetAlbumData(uuid) {
   let config = immichConfig();
   let albumUrl = `${config.url}/api/albums/${uuid}`;
 
-  let albumData = await EleventyFetch(albumUrl, {
+  return await EleventyFetch(albumUrl, {
     duration: "10m",
     type: "json",
     fetchOptions: {
@@ -25,29 +25,15 @@ async function immichAlbumShortcode(uuid) {
       ...{ accept: 'application/json' }
     }
   });
-
-  let html = `<div class="immich-album"><h2>${albumData.albumName}</h2>`;
-  if (albumData.description) {
-    html += `<p>${albumData.description}</p>`;
-  }
-  if (albumData.assets.length) {
-    html += '<div class="immich-album-assets">';
-    for (let asset of albumData.assets) {
-      html += await immichImageShortcode(asset.id);
-    }
-    html += '</div>';
-  }
-  html += '</div>';
-  return html;
 }
 
 /**
- * Fetches image data and file from a given UUID and generates HTML for the image element with specified attributes.
+ * Fetches image data for a given Immich asset UUID.
  *
  * @param {string} uuid - The UUID of the image asset.
- * @returns {Promise<string>} - A promise that resolves to the generated HTML for the image element.
+ * @returns {Promise<Object>} - An object containing the image data and file.
  */
-async function immichImageShortcode(uuid) {
+async function immichGetImageData(uuid) {
   let config = immichConfig();
   let assetDataUrl = `${config.url}/api/assets/${uuid}`;
   let assetFileUrl = `${config.url}/api/assets/${uuid}/original`;
@@ -67,9 +53,49 @@ async function immichImageShortcode(uuid) {
     fetchOptions: fetchOptions
   });
 
-  let alt = assetData.exifInfo.description ?? 'No image description available';
+  return {
+    assetData: assetData,
+    assetFile: assetFile
+  };
+}
 
-  let metadata = await Image(assetFile, {
+/**
+ * Render an album with images for each asset.
+ *
+ * @param {Object} album - The album to render.
+ * @param {string} album.albumName - The name of the album.
+ * @param {string} [album.description] - The description of the album.
+ * @param {Array} album.assets - The assets in the album.
+ * @param {string} album.assets[].id - The ID of the asset.
+ *
+ * @return {Promise<string>} The HTML string representing the rendered album.
+ */
+async function immichRenderAlbum(album) {
+  let html = `<div class="immich-album"><h2>${album.albumName}</h2>`;
+  if (album.description) {
+    html += `<p>${album.description}</p>`;
+  }
+  if (album.assets.length) {
+    html += '<div class="immich-album-assets">';
+    for (let asset of album.assets) {
+      html += await immichImageShortcode(asset.id);
+    }
+    html += '</div>';
+  }
+  html += '</div>';
+  return html;
+}
+
+/**
+ * Renders an image with optional description and returns the generated HTML.
+ *
+ * @param {object} image - The image object to render.
+ * @returns {Promise<string>} - A promise that resolves with the generated HTML.
+ */
+async function immichRenderImage(image) {
+  let alt = image.assetData.exifInfo.description ?? 'No image description available';
+
+  let metadata = await Image(image.assetFile, {
     widths: [300, 600],
     formats: ["jpeg"],
     outputDir: 'public/media/img/',
@@ -86,6 +112,43 @@ async function immichImageShortcode(uuid) {
   return Promise.resolve(Image.generateHTML(metadata, attributes));
 }
 
+/**
+ * Fetches an album and renders each asset.
+ *
+ * @param uuid
+ * @returns {Promise<string>}
+ */
+async function immichAlbumShortcode(uuid) {
+  let album = await immichGetAlbumData(uuid);
+  return immichRenderAlbum(album);
+}
+
+/**
+ * Fetches image data and file from a given UUID, generates HTML for image element.
+ *
+ * @param {string} uuid - The UUID of the image asset.
+ * @returns {Promise<string>} - A promise that resolves to the generated HTML for the image element.
+ */
+async function immichImageShortcode(uuid) {
+  let image = await immichGetImageData(uuid);
+  return immichRenderImage(image);
+}
+
+/**
+ * Retrieves the Immich configuration settings required for the plugin.
+ *
+ * Verifies that required environment variables are set and throws error if any are missing.
+ *
+ * Returns an object containing Immich base URL, API key, and default fetch options.
+ *
+ * @throws {Error} If any of the required environment variables are missing, an error is thrown.
+ *
+ * @returns {Object} An object containing the following properties:
+ * - url {string} - The Immich base URL.
+ * - apikey {string} - The Immich API key.
+ * - defaultFetchOptions {Object} - The default fetch options for making requests to Immich API.
+ *   - headers {Object} - The headers for requests, including the 'x-api-key' header with the API key.
+ */
 function immichConfig() {
   let required_vars = ['IMMICH_BASE_URL', 'IMMICH_API_KEY'];
   for (let name of required_vars) {
@@ -105,6 +168,13 @@ function immichConfig() {
   };
 }
 
+/**
+ * Adds Immich-related configurations to Eleventy.
+ *
+ * Validates Immich connectivity.
+ *
+ * @param {Object} eleventyConfig - The Eleventy config object.
+ */
 function EleventyImmich(eleventyConfig) {
   let config = immichConfig();
 
